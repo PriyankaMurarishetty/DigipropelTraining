fdf2115899cc2dd074e9b732fec341de7f183f82
PriyankaMurarishetty <86397800+PriyankaMurarishetty@users.noreply.github.com>
2021-06-24 12:16:02 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice1
+{
+    class Class1
+    {
+        static string employeename = "Priyanka";
+        public void DisplayName()
+        {
+            Method();
+            Console.WriteLine(employeename);
+        }
+        public void Method()
+        {
+            Console.WriteLine("Welcome to Digipropel");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice1
+{
+    class Program
+    {
+        static int employeeid = 0194;
+        
+        static void Main(string[] args)
+        {
+            Class1 display = new Class1();
+            display.DisplayName();
+            Console.WriteLine(employeeid);
+            //DisplayName();
+            //Console.WriteLine(employeename);
+        }
+
+    }
+}
