2ea97513ac5c472660cf4644c3b5e75ceaf0b87f
PriyankaMurarishetty <86397800+PriyankaMurarishetty@users.noreply.github.com>
2021-07-01 20:01:01 +0530

    Add files via upload

---
+++ June28_7.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Assignment2
+{
+    class June28_7
+    {
+        public void Missingnum()
+        {
+            int[] num1= { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            Console.WriteLine("original numbers in the array");
+            Array.ForEach(num1, Console.WriteLine);
+            Console.WriteLine("the missing number in the array is:", +test(num1,num1));
+            int[] num2 = { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            Console.WriteLine("\noriginal numbers in the array");
+            Array.ForEach(num2, Console.WriteLine);
+            Console.WriteLine("the missing number in the array:" + test(num1,num2));
+            int[] num3 = {10, 11, 12, 13, 15, 16, 17, 18, 19, 20 };
+            Console.WriteLine("the original array elements");
+            Array.ForEach(num3, Console.WriteLine);
+            Console.WriteLine("the missing number in the array " + test(num1,num3));
+            int[] num4 = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+            Console.WriteLine("original array elements");
+            Array.ForEach(num4, Console.WriteLine);
+            Console.WriteLine("the missing number in the array"+test(num1,num4));
+
+        }
+        public static int test(int[]brr,int[]brr1)
+        {
+            return brr.Sum() - brr1.Sum();
+        }
+    }
+}
---
+++ June28_8.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2
+{
+    class June28_8
+    {
+        public void add()
+        {
+            int i, n1, n2, temp1, temp2, sum=0;
+          
+            Console.WriteLine("enter the number of elements in first array:");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the number of elements in second array");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            int[] arr1 = new int[n1];
+          int[] arr2 = new int[n2];
+            Console.WriteLine("enter the elements into first array:");
+            for(i=0;i<n1;i++)
+            {
+                Console.WriteLine("elements:", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            Console.WriteLine("enter the elements into second array:");
+            for (i = 0; i < n2; i++)
+            {
+                Console.WriteLine("elements:", i);
+                arr2[i] = Convert.ToInt32(Console.ReadLine());
+            } 
+            string[] strarr1 = Array.ConvertAll(arr1, ele => ele.ToString());
+            string[] strarr2 =Array.ConvertAll(arr2, ele => ele.ToString());
+            temp1 = Convert.ToInt32(string.Join ("",strarr1));
+            temp2 = Convert.ToInt32(string.Join("",strarr2));
+            sum = temp1 + temp2;
+            Console.WriteLine("{0}+{1}={2}",temp1,temp2,sum);
+
+        }
+    }
+}
