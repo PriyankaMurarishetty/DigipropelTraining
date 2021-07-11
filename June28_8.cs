29c5231377d4dca69a439b412d625618271663ed
PriyankaMurarishetty <86397800+PriyankaMurarishetty@users.noreply.github.com>
2021-07-11 12:49:27 +0530

    Update June28_8.cs

--- June28_8.cs
+++ June28_8.cs
@@ -16,26 +16,32 @@ namespace Assignment2
             n2 = Convert.ToInt32(Console.ReadLine());
             int[] arr1 = new int[n1];
           int[] arr2 = new int[n2];
+            //initializing first array elements
             Console.WriteLine("enter the elements into first array:");
             for(i=0;i<n1;i++)
             {
                 Console.WriteLine("elements:", i);
                 arr1[i] = Convert.ToInt32(Console.ReadLine());
             }
-            
+            //initializing second array elements
             Console.WriteLine("enter the elements into second array:");
             for (i = 0; i < n2; i++)
             {
                 Console.WriteLine("elements:", i);
                 arr2[i] = Convert.ToInt32(Console.ReadLine());
             } 
+            //converting first and second array elements to string
             string[] strarr1 = Array.ConvertAll(arr1, ele => ele.ToString());
             string[] strarr2 =Array.ConvertAll(arr2, ele => ele.ToString());
             temp1 = Convert.ToInt32(string.Join ("",strarr1));
             temp2 = Convert.ToInt32(string.Join("",strarr2));
+            //adding the array's
             sum = temp1 + temp2;
             Console.WriteLine("{0}+{1}={2}",temp1,temp2,sum);
 
         }
     }
 }
+/*input:first array elemeents{1,6,9}
+        second array elements{2,3}
+  output:sum=169+23=192
