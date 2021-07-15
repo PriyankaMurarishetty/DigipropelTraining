d5d745e5a77590da2c120aa8552c8fa90e1a7be1
PriyankaMurarishetty <86397800+PriyankaMurarishetty@users.noreply.github.com>
2021-07-15 10:33:22 +0530

    Add files via upload

---
+++ July05_7.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+   
+
+namespace Assignment4._1
+    {
+ 
+class July05_7
+    {
+        const string fileName = "AppSettings.dat"; // defining constant string
+        static void Main()
+        {
+           WriteDefaultValues();
+            DisplayValues();
+            
+        }
+
+        public static void WriteDefaultValues()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+
+                writer.Write("current date and time : " + DateTime.Now.ToString());
+
+            }
+        }
+
+        public static void DisplayValues()
+        {
+
+
+            if (File.Exists(fileName))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+                {
+                    Console.WriteLine(reader.ReadString());
+
+                }
+
+
+            }
+        }
+    }
+}
+/* output :
+   current date and time : 7/12/2021  7:30:22 PM*/
---
+++ July05_8.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace Assignmnet4._2
+{
+    class July05_8
+    {
+        static void Main(string[] args)
+        { 
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+
+                Console.WriteLine("Enter entire location of file along with file extension:"); // to take input from user
+                string filename;
+        filename = Console.ReadLine();
+
+                //   using (StreamReader sr = new StreamReader("D:/Priyanka.txt"))
+                using (StreamReader sr = new StreamReader(filename))
+
+                {
+                    string line;
+                    // Read and display lines from the file until 
+                    // the end of the file is reached. 
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+}
+            }
+            catch
+            (Exception e) // if the file doesn't found it will pass exception message
+{
+    Console.WriteLine("The File Couldnot find");
+    Console.WriteLine(e.Message);
+
+}
+Console.ReadKey();
+
+        }
+    }
+}
+/*output:
+Enter entire location of file along with file extension:
+C:\Users\dpladmin\Desktop\Priyanka.txt
+Hello              */
